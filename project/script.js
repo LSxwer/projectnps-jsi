bec585fbd63137807e6ea47d200e6a7c19981900
LSxwer <126370973+LSxwer@users.noreply.github.com>
2023-12-03 11:57:06 +0700

    Add files via upload

--- project/script.js
+++ project/script.js
@@ -1,16 +1,20 @@
-const data = null;
+const apiKey = 'a568f8e404mshe6cd3ddc4962405p1fc956jsn94cec1132327'; // Thay thế YOUR_RAPIDAPI_KEY bằng khóa API thực của bạn
 
-const xhr = new XMLHttpRequest();
-xhr.withCredentials = true;
-
-xhr.addEventListener('readystatechange', function () {
-	if (this.readyState === this.DONE) {
-		console.log(this.responseText);
+const url = 'https://spotify23.p.rapidapi.com/tracks/?ids=4WNcduiCmDNfmTEz7JvmLv';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': "a568f8e404mshe6cd3ddc4962405p1fc956jsn94cec1132327",
+		'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
 	}
-});
+};
 
-xhr.open('GET', 'https://spotify23.p.rapidapi.com/search/?q=%3CREQUIRED%3E&type=multi&offset=0&limit=10&numberOfTopResults=5');
-xhr.setRequestHeader('X-RapidAPI-Key', 'a568f8e404mshe6cd3ddc4962405p1fc956jsn94cec1132327');
-xhr.setRequestHeader('X-RapidAPI-Host', 'spotify23.p.rapidapi.com');
-
-xhr.send(data);
+fetch(url, options)
+	.then(response => response.text())
+	.then(result => console.log(result))
+	.then((result) => {
+		console.log(result); //// kết quả là gì
+		
+		
+	  })
+	  .catch(error => console.error(error));
